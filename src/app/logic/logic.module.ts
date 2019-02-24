4b8944935723504e6e48222308be9ab953ee0fb0
Dominik <dominik.kuzmicz@gmail.com>
2019-02-24 13:23:55 +0100

    Feat - Add Movie components, add child routing

--- src/app/logic/logic.module.ts
+++ src/app/logic/logic.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageHomeComponent } from './components/page-home/page-home.component';
-import { PageContactComponent } from './components/page-contact/page-contact.component';
+import { PageVideoListComponent } from './components/page-video-list/page-video-list.component';
+import { PageVideoNotFoundComponent } from './components/page-video-not-found/page-video-not-found.component';
+import { PageVideoProfileComponent } from './components/page-video-profile/page-video-profile.component';
+import { LogicRoutingModule } from './logic-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [PageHomeComponent, PageContactComponent],
+  declarations: [PageVideoListComponent, PageVideoNotFoundComponent, PageVideoProfileComponent],
   imports: [
-    CommonModule
-  ],
-  exports: [PageHomeComponent, PageContactComponent]
+    CommonModule,
+    LogicRoutingModule,
+    RouterModule
+  ]
+  //exports: [PageHomeComponent, PageContactComponent]
 })
 export class LogicModule { }
