a3f2a910779e314ae08824d1718060b26d212733
Dominik <dominik.kuzmicz@gmail.com>
2019-02-24 12:35:44 +0100

    Fix - Impot RouterModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from '../app-routing.module';
 import { MenuComponent } from './components/menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -9,7 +9,7 @@ import { MenuComponent } from './components/menu/menu.component';
   ],
   imports: [
     CommonModule,
-    AppRoutingModule
+    RouterModule
   ], 
   exports: [MenuComponent]
 })
